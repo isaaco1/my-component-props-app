9a51bfa9b7f126f9afd099e3b5b553b79f5b8e1e
isaaco1 <isaacorzech@gmail.com>
2023-11-04 22:16:33 +0000

    Updated React-DOM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Jumbotron } from "./components/Jumbotron";
 
 export const App = () => {
-  <div className="Container">
-    <Jumbotron />
-  </div>;
+  return (
+    <div className="Container">
+      <Jumbotron />
+    </div>
+  );
 };
